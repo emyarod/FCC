30b233c7f85f4380ab11aa87f781a942228e5ebd
emyarod <allllaboutyou@gmail.com>
2016-08-05 15:50:42 -0700

    Add mutation.js

---
+++ Challenges/02-Basic Algorithm Scripting/mutation.js
@@ -0,0 +1,14 @@
+function mutation(arr) {
+  const [first, second] = arr;
+  let counter = 0;
+  for (var i = 0; i < second.length; i++) {
+    if (first.toLowerCase().indexOf(second[i].toLowerCase()) === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+mutation(['hello', 'hey']);
+console.log(mutation(['hello', 'hey']));
