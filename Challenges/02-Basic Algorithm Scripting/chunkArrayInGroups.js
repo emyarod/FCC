697e1be26a4798a73deece6d8f1e5d7ff3309ac3
emyarod <allllaboutyou@gmail.com>
2016-08-05 15:32:36 -0700

    Add chunkArrayInGroups.js

---
+++ Challenges/02-Basic Algorithm Scripting/chunkArrayInGroups.js
@@ -0,0 +1,11 @@
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+
+  return newArr;
+}
+
+chunkArrayInGroups(['a', 'b', 'c', 'd'], 2);
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2));
