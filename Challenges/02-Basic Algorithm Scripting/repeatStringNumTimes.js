45909f4cc122e2b1bca3ee15243e792ee0b6ee03
emyarod <allllaboutyou@gmail.com>
2016-08-04 19:00:16 -0700

    Add repeatStringNumTimes.js

--- Challenges/02-Basic Algorithm Scripting/repeatStringNumTimes.js
+++ Challenges/02-Basic Algorithm Scripting/repeatStringNumTimes.js
@@ -1,6 +1,12 @@
 function repeatStringNumTimes(str, num) {
-  // repeat after me
-  return str;
+  const origStr = str;
+  let newstr = '';
+  for (let i = 0; i < num; i++) {
+    newstr += origStr;
+  }
+
+  return newstr;
 }
 
 repeatStringNumTimes('abc', 3);
+console.log(repeatStringNumTimes('abc', 3));
