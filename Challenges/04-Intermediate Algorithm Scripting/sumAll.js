a00307230c0761c495f5af7e362cb0219d17c571
emyarod <allllaboutyou@gmail.com>
2016-08-25 00:41:47 -0700

    Add sumAll.js

---
+++ Challenges/04-Intermediate Algorithm Scripting/sumAll.js
@@ -0,0 +1,15 @@
+function sumAll(arr) {
+  const max = Math.max.apply(null, arr);
+  const min = Math.min.apply(null, arr);
+  const array = [];
+  for (var i = min; i <= max; i++) {
+    array.push(i);
+  }
+
+  return array.reduce((previous, current, index, array) => {
+    return previous + current;
+  });
+}
+
+sumAll([1, 4]);
+console.log(sumAll([1, 4]));
