79bd26dd9a61d191d943334c48e6621c18d28908
emyarod <allllaboutyou@gmail.com>
2016-08-26 00:20:58 -0700

    Add sumPrimes.js

---
+++ Challenges/04-Intermediate Algorithm Scripting/sumPrimes.js
@@ -0,0 +1,28 @@
+function isPrime(value) {
+  for (let i = 2; i < value; i++) {
+    if (!(value % i)) {
+      return false;
+    }
+  }
+
+  return value > 1;
+}
+
+function sumPrimes(num) {
+  const primes = [];
+  let total = 0;
+  for (var i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  primes.forEach((prime) => {
+    total += prime;
+  });
+
+  return total;
+}
+
+sumPrimes(10);
+console.log(sumPrimes(977));
