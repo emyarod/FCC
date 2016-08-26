708e649c8d64c403beb7b2088c0b907935c402e2
emyarod <allllaboutyou@gmail.com>
2016-08-25 20:08:28 -0700

    add fearNotLetter.js

---
+++ Challenges/04-Intermediate Algorithm Scripting/fearNotLetter.js
@@ -0,0 +1,34 @@
+function fearNotLetter(str) {
+  const charCodes = [];
+  const range = [];
+  for (var i = 0; i < str.length; i++) {
+    charCodes.push(str.charCodeAt(i));
+  }
+
+  let missing = 0;
+  let found = false;
+
+  range.push(charCodes[0]);
+  charCodes.reduce((previousValue, currentValue, currentIndex, array) => {
+    if (!found) {
+      if (currentValue === (previousValue + 1)) {
+        missing = previousValue;
+        found = true;
+        range.push(currentValue);
+        return currentValue;
+      }
+
+      range.push(previousValue + 1);
+      return currentValue + 1;
+    }
+  }, charCodes[0]);
+
+  if (!missing) {
+    return undefined;
+  }
+
+  return String.fromCharCode(missing);
+}
+
+fearNotLetter("abcdefghjklmno");
+console.log(fearNotLetter("abcdefghjklmno"));
