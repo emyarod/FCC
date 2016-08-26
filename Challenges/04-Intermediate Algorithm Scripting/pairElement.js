baf76fcd38257e207b62986028b4a9341a96e838
emyarod <allllaboutyou@gmail.com>
2016-08-25 19:45:32 -0700

    Add pairElement.js

---
+++ Challenges/04-Intermediate Algorithm Scripting/pairElement.js
@@ -0,0 +1,18 @@
+function pairElement(str) {
+  const pairs = [];
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === 'A') {
+      pairs.push([str[i], 'T']);
+    } else if (str[i] === 'T') {
+      pairs.push([str[i], 'A']);
+    } else if (str[i] === 'C') {
+      pairs.push([str[i], 'G']);
+    } else if (str[i] === 'G') {
+      pairs.push([str[i], 'C']);
+    }
+  }
+
+  return pairs;
+}
+
+pairElement("GCG");
